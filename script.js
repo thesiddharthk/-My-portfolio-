3bb22251bb72086d43f1ed28f5a2b9e590257456
thesiddharthk <siddharthishu06@gmail.com>
2026-06-09 22:51:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+function scrollToSection() {
+
+  document.querySelector(".skills-section")
+    .scrollIntoView({
+      behavior: "smooth"
+    });
+
+}
+
+function openEmail() {
+
+  window.location.href =
+    "mailto:siddharthishu06@gmail.com";
+
+}
+
+function openGithub() {
+
+  window.open(
+    "https://github.com/ArjunreddylovesPreethi",
+    "_blank"
+  );
+
+}
+
+function showSlack() {
+
+  document.getElementById("slackText")
+    .innerHTML =
+    "Slack ID: Arjun Reddy loves Preethi";
+
+}
