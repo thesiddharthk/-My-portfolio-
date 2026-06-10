b7c195db4de48ead9441198bd7ce4a470ee07d5d
Siddharth K. <siddharthishu06@gmail.com>
2026-06-10 18:12:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,42 @@ function showSlack() {
     "Slack ID: U0B16L9UR99";
 
 }
+
+// Typing Animation
+
+const words = [
+  "Writer",
+  "Philosopher",
+  "Photographer",
+  "Movie Analyst",
+  "People Connector"
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+
+function typeEffect() {
+
+  const currentWord = words[wordIndex];
+
+  document.getElementById("typing").textContent =
+    currentWord.substring(0, charIndex);
+
+  charIndex++;
+
+  if (charIndex > currentWord.length) {
+
+    setTimeout(() => {
+
+      charIndex = 0;
+      wordIndex =
+        (wordIndex + 1) % words.length;
+
+    }, 1200);
+
+  }
+
+  setTimeout(typeEffect, 120);
+}
+
+typeEffect();
