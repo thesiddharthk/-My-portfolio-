9c020df5d8534e4b6b90cc934769c9146281e99c
Siddharth K. <siddharthishu06@gmail.com>
2026-06-13 18:59:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -152,3 +152,16 @@ topBtn.onclick = ()=>{
 
 };
 
+const glow =
+document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove",(e)=>{
+
+  glow.style.left =
+  e.clientX + "px";
+
+  glow.style.top =
+  e.clientY + "px";
+
+});
+
