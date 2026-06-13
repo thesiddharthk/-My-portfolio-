44836b8bf37b94ff4ed9b86b5e0cf470ca10faa6
Siddharth K. <siddharthishu06@gmail.com>
2026-06-13 18:55:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -125,3 +125,30 @@ window.addEventListener("scroll",()=>{
   progress + "%";
 
 });
+
+const topBtn =
+document.getElementById("topBtn");
+
+window.addEventListener("scroll",()=>{
+
+  if(window.scrollY > 400){
+
+    topBtn.style.display = "block";
+
+  }else{
+
+    topBtn.style.display = "none";
+
+  }
+
+});
+
+topBtn.onclick = ()=>{
+
+  window.scrollTo({
+    top:0,
+    behavior:"smooth"
+  });
+
+};
+
